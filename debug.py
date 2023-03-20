b53f258c466d2056654b3164bd1642bbf281899f
Adam La Rosa <adam.larosa@flatironschool.com>
2023-03-20 11:28:16 -0700

    debug notes

--- debug.py
+++ debug.py
@@ -4,6 +4,14 @@ from lib import *
 
 seeds() # <--- allows us to start with data in memory
 
+# Not all models may have all the attributes or properties they need!  The 
+# starter code provided allows for inital data but it's up to you impliment any
+# additional functionality to meet the deliverables.
+
+# c1 = Client.all[0]
+# c1.rentals ????
+# c1.vhs ?????
+# c1.genres ??!?!?!  ;)
 
 
 
